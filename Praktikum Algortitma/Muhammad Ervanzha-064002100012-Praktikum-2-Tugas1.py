27b02675c67e774bc24603e26db6181c019095b6
ervanzha <91467311+ervanzha@users.noreply.github.com>
2021-09-27 18:16:37 +0700

    commit ppraktikum 2 tugas 1

---
+++ Praktikum Algortitma/Muhammad Ervanzha-064002100012-Praktikum-2-Tugas1.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 27 13:43:05 2021
+
+@author: ervan
+
+
+"""
+
+import math
+a= float(input("masukan nilai a :"))
+b= float(input("masukan niali b :"))
+
+
+hasil = a + b 
+print("jumlah a+b adalah",hasil)
+hasil = a - b
+print("jumlah a-b adalah",abs(hasil))
+hasil = a * b
+print("jumlah a*b adalah",hasil)
+hasil = a % b
+print("jumlah a%b adalah",hasil)
+hasil = a / b
+print("jumlah a/b adalah",hasil)
+hasil = a ** b
+print("jumlah a**b adalah",hasil)
+hasil = math.log10(a)
+print("jumlah math.log 10(a)",hasil) 
